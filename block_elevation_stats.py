0ba0a62245b083ebd2626e97b3ff75576a46417e
Lasercar7 <lasercar@users.noreply.github.com>
2014-10-22 11:33:35 -0500

    plan

--- block_elevation_stats.py
+++ block_elevation_stats.py
@@ -1,2 +1,10 @@
 # Copyright (c) 2014 Lasercar7 (@lasercar) - MIT License
 # http://lasercar.github.io
+
+#import
+
+#dictionary that maps block ids (to be collected) with readable names
+
+#iterate through mcedit selection, adding collected freq data to object/array
+
+#open csv file, write object to it
