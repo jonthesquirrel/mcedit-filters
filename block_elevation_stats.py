6c6d133ad3463eedc490d0128c77e7c224dd384b
Lasercar7 <lasercar@users.noreply.github.com>
2014-10-27 14:49:25 -0500

    iteration test

--- block_elevation_stats.py
+++ block_elevation_stats.py
@@ -1,10 +1,19 @@
 # Copyright (c) 2014 Lasercar7 (@lasercar) - MIT License
 # http://lasercar.github.io
 
-#import
-
 #dictionary that maps block ids (to be collected) with readable names
 
-#iterate through mcedit selection, adding collected freq data to object/array
+#iterate through world, adding collected freq data to object/array
+#def perform(level, box, options):
+#	level.blockAt(x, y, x,)
+data = [['elevation:', 1, 2, 3, 4, 5], ['Coal', 0, 1, 2, 3, 4], ['Iron', 2, 2, 2, 2, 2], ['Diamond', 4, 3, 2, 1, 0]]
 
 #open csv file, write object to it
+from os.path import expanduser
+filepath = expanduser('~') + '/Downloads'
+
+#iteration test
+for column in range(len(data)):
+	print ''
+	for row in range(len(data[column])):
+		print data[column][row]
